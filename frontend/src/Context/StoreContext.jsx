50ecf689cc77ac4ce5456eff963f2115ad49a131
abhi1256829 <ag1256829@gmail.com>
2025-11-21 02:09:49 +0530

    Update StoreContext.jsx

--- frontend/src/Context/StoreContext.jsx
+++ frontend/src/Context/StoreContext.jsx
@@ -7,7 +7,7 @@ const StoreContext = createContext(null);
 const StoreContextProvider = (props) => {
 
     const[cartItems,setCartItems] = useState({});
-    const url = "http://localhost:4000"
+    const url = "https://food-menu-mvnl.onrender.com"
     const [token,setToken] = useState("");
     const [food_list,setFoodList] = useState([]);
 
